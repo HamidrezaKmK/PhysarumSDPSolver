c79717a8e7f71ed5a95eb90eb775db11a2238c2d
hamidrezakamkari <hamidrezakamkari@gmail.com>
2020-10-14 01:20:58 +0330

    tester updated

--- tester/tester.sh
+++ tester/tester.sh
@@ -4,15 +4,14 @@
 # ${3} test file(s) in testData as regex,   e.g. testData/sdplip/*.in => regex = "sdplip/*.in"
 cd ../
 mkdir -p out
-dir_list=(`ls ${3}`) ;
-#src_directory=(`echo "${2}" | sed 's/[^/]*$//'`) ;
-test_directory=(`echo "${3}" | sed 's/[^/]*$//'`) ;
-test_directory="testData/$test_directory"
+dir_list=(`ls "testData/${3}"`) ;
+test_directory=(`echo "testData/${3}" | sed 's/[^/]*$//'`) ;
 output_directory="out/" ;
 
 for test_list in ${dir_list[*]};
 do
 test_name=(`echo "$test_list" | awk -F "/" '{print $NF}'`) ;
+echo "${1} ${2} $test_directory$test_name"
 "${1} ${2}" < "$test_directory$test_name" > "$output_directory$test_name.out"
 done
 
