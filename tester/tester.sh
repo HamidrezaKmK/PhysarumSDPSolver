c83a6cba9cea1ad8c6457c9e72b73016277900ee
Keivan <k1rezaei78@gmail.com>
2020-10-13 21:22:00 +0330

    fix tester

--- tester/tester.sh
+++ tester/tester.sh
@@ -2,21 +2,21 @@
 # ${1} .exe folder,   src/main.exe
 # ${2} test folder,   testData/sdplip/*.in
 cd ../
-mkdir -p out
-dir_list=(`ls ${2}`) ;
-src_directory=(`echo "${1}" | sed 's/[^/]*$//'`) ;
-test_directory=(`echo "${2}" | sed 's/[^/]*$//'`) ;
-output_directory="out/" ;
+									   mkdir -p out
+									   dir_list=(`ls ${2}`) ;
+									   src_directory=(`echo "${1}" | sed 's/[^/]*$//'`) ;
+									   test_directory=(`echo "${2}" | sed 's/[^/]*$//'`) ;
+									   output_directory="out/" ;
 
-echo $src_directory ;
-echo $test_directory ;
+									   echo $src_directory ;
+									   echo $test_directory ;
 
-g++ "$1" -O2 -c -o "${src_directory}SDPSolver".o && g++ "${src_directory}main.cpp" "${src_directory}SDPSolver".o -O2 -o "${src_directory}main"
+									   g++ "$1" -O2 -c -o "${src_directory}SDPSolver".o && g++ "${src_directory}main.cpp" "${src_directory}SDPSolver".o -O2 -o "${src_directory}main"
 
-echo "Done!" ;
+									   echo "Done!" ;
 
-for test_list in ${dir_list[*]};
-do
-   test_name=(`echo "$test_list" | awk -F "/" '{print $NF}'`) ;
-   "${src_directory}main" < "../$test_directory$test_name" > "$output_directory$test_name.out"
-done
+									   for test_list in ${dir_list[*]};
+									   do
+									   test_name=(`echo "$test_list" | awk -F "/" '{print $NF}'`) ;
+									   "${src_directory}main" < "$test_directory$test_name" > "$output_directory$test_name.out"
+									   done
