52bc871ba0daafd2c2e91d2555dced03f8338b09
hamidrezakamkari <hamidrezakamkari@gmail.com>
2020-10-19 22:10:43 +0330

    tester.sh is now tidy!

--- tester/tester.sh
+++ tester/tester.sh
@@ -12,8 +12,19 @@ output_directory="out/" ;
 for test_list in ${dir_list[*]};
 do
 test_name=(`echo "$test_list" | awk -F "/" '{print $NF}'`) ;
-echo "${1} ${2} $test_directory$test_name"
-"${1}" "${2}" < "$test_directory$test_name" > "$output_directory$test_name.out"
+echo "Running $test_directory$test_name with implementation=${2} ..."
+"${1}" "${2}" < "$test_directory$test_name" > "$output_directory$test_name.out" 2> "$output_directory$test_name.err"
+
+if [ $? -eq 0 ]; then
+    echo
+    echo "[DONE] Run successful!"
+    echo -e "\tcerr output available at \"$output_directory$test_name.err\"\n"
+    echo -e "\tcout output available at \"$output_directory$test_name.out\"\n\n"
+else
+    echo
+    echo "[ERROR]: Run unsuccessful at \"$test_directory$test_name\""
+    echo -e "\tFor more information check out \"$output_directory$test_name.err\"\n\n"
+fi
 done
 
 read -n 1 -r -s -p $'Press enter to continue...\n'
